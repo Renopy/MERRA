8c432d1d05367cdcb912adab892ac3efcb5996e8
Rezanosratpour <93834390+Rezanosratpour@users.noreply.github.com>
2022-08-04 21:56:18 +0430

    Update Read_MERRA.py

--- Read_MERRA.py
+++ Read_MERRA.py
@@ -4,13 +4,12 @@ import netCDF4
 
 fp = 'insert your file path here'
 
-    
+   
 def read_merra(fp):
     NETCDF = { }
-    
     nc = netCDF4.Dataset(fp)
     Var = nc.variables
-    Keys = Var.keys
+    Keys = list(Var)
     for k in Keys: 
         array = numpy.array(nc[k])
         NETCDF[k] =   numpy.flip(array,axis=0)
@@ -18,4 +17,4 @@ def read_merra(fp):
         NETCDF['lon'] = ( nc.SouthernmostLatitude  , nc.NorthernmostLatitude ) 
         NETCDF['res'] =  { 'x'  : nc.LatitudeResolution  , 'y' : nc.LongitudeResolution }
     return NETCDF
-     
+
